520f3336ae9e221983762e341ac06a331415e5af
Frosty <biztos@mac.com>
2016-01-08 23:17:29 +0100

    package-level too

--- logdemo.go
+++ logdemo.go
@@ -17,7 +17,21 @@ func New(name string) *LoggingThing {
 	return &LoggingThing{Logger: log.New(os.Stdout, prefix, flags)}
 }
 
-// Log writes logs something for the LoggingThing.
+// Log logs something for the LoggingThing.
 func (lt *LoggingThing) Log(msg string) {
 	lt.Logger.Print(msg)
+}
+
+// OK, that's all well and good, but what, you ask, if I'm too lazy to set
+// up a structure and whatnot?
+//
+// In that case you should be able to use a package variable; just be sure
+// to expose it by making it begin with an Uppercase letter.
+var logger_prefix = "global "
+var logger_flags = log.Ldate | log.Ltime | log.Lmicroseconds
+var Logger = log.New(os.Stdout, logger_prefix, logger_flags)
+
+// Log logs something out of the blue.
+func Log(msg string) {
+	Logger.Print(msg)
 }
